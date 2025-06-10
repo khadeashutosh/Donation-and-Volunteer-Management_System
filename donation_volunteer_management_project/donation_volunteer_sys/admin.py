fbc0790abcecee17d957ad3869c3dabe822e2193
Ashu <ashutoshkhade2530@gmail.com>
2025-06-10 20:30:15 +0530

    Configure Django admin interface for project models

--- donation_volunteer_management_project/donation_volunteer_sys/admin.py
+++ donation_volunteer_management_project/donation_volunteer_sys/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Donor, Volunteer , DonationArea, Donation, Gallery
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ('id','user', 'contact', 'address', 'regdate')
 
-# Register your models here.
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin):
+  list_display=('id','user', 'contact','address','regdate')
+
+@admin.register(DonationArea)
+class DonatonAreaAdmin(admin.ModelAdmin):
+  list_display=('id','areaname','description','creationdate')
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+   list_display=('id','donor','volunteer','donationarea','donationname')
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+   list_display=('id','donation','deliverypic','creationdate',)
